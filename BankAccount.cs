d4ca9df77f818ca8d27045dd9b5e246423969ad2
Bozhidar-Yovchev <bozhidar601@gmail.com>
2024-09-12 18:38:43 +0300

    Bank account deposit and withdraw

---
+++ BankAccount.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace БанковаСметка
+{
+    public class BankAccount
+    {
+        private string accountNumber;
+        private string ownerName;
+        private decimal accountBalance;
+
+        public string AccountNumber
+        {
+            get { return this.accountNumber; }
+            set { this.accountNumber = value; }
+        }
+        public string OwnerName
+        {
+            get { return this.ownerName; }
+            set { this.ownerName = value; }
+        }
+        public decimal AccountBalance
+        {
+            get { return this.accountBalance; }
+            set { this.accountBalance = value; }
+        }
+
+        public BankAccount()
+        {
+            this.AccountNumber = "";
+            this.OwnerName = "";
+            this.AccountBalance = 0.0M;
+        }
+        public BankAccount(string accountNumber,string ownerName,decimal accountBalance)
+        {
+            this.AccountNumber = accountNumber;
+            this.OwnerName = ownerName;
+            this.AccountBalance = accountBalance;
+        }
+
+        public string MakeDeposit(decimal sum)
+        {
+            this.AccountBalance += sum;
+           return $"Account balance: {AccountBalance}";
+        }
+        public string MakeWithdraw(decimal withdraw)
+        {
+            if (this.AccountBalance - withdraw <= 0)
+            {
+                return "insuffisant amount of money";
+            }
+            else
+            {
+                this.AccountBalance -= withdraw;
+                return $"Withdrawed money: {withdraw}. You have in your bank account: {this.AccountBalance}";
+            }  
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace БанковаСметка
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string[] customer = Console.ReadLine().Split();
+            string accountNumber = customer[0];
+            string ownerName = customer[1] + " " + customer[2];
+            decimal accountBalance = decimal.Parse(customer[3]);
+
+            BankAccount account = new BankAccount(accountNumber, ownerName, accountBalance);
+            string command = Console.ReadLine();
+
+            while (command.ToLower() != "end")
+            {
+                string[] masiv = command.Split();
+                decimal sum = decimal.Parse(masiv[1]);
+                switch (masiv[0].ToLower())
+                {
+                    case "deposit":
+                        Console.WriteLine(account.MakeDeposit(sum));
+                        break;
+                    case "withdraw":
+                        Console.WriteLine(account.MakeWithdraw(sum));
+                        break;
+                    default:
+                        Console.WriteLine("There is no such a command");
+                        break;
+                }
+                command = Console.ReadLine().ToLower();
+            }
+            
+
+        }
+    }
+}
